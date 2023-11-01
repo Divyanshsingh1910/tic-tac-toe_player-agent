80cca02a866c1c123ae40a8ea4a4d0ff863b23f9
Trijal <trijalsrivastava30@gmail.com>
2023-11-01 15:25:33 +0530

    added the function in helper_functions

--- Helper_functions.py
+++ Helper_functions.py
@@ -1,5 +1,10 @@
 #importing library
 import numpy as np
+import os
+import random
+import pandas as pd 
+from PIL import Image
+import matplotlib.pyplot as plt
 
 """
 Functions:
@@ -39,9 +44,65 @@ Functions:
                 This will also change accordingly         
             #modification : user_input will be a pair of indices on where to play and the character to play
 """
+def choose_image_0():
+        folder_0 = r'C:\Users\Trijal Srivastava\OneDrive\Desktop\VS CODE\ICG_competition\tic-tac-toe_player-agent\Data\Images\zeros'
+        files_0 = os.listdir(r'C:\Users\Trijal Srivastava\OneDrive\Desktop\VS CODE\ICG_competition\tic-tac-toe_player-agent\Data\Images\zeros')
+        image_files_0 = [f for f in files_0 if f.endswith(('.png'))]
+        random_image_0 = random.choice(image_files_0)
+        image_path_0 = os.path.join(folder_0, random_image_0)
+        return image_path_0
+    
+def choose_image_1():
+        folder_1 = r'C:\Users\Trijal Srivastava\OneDrive\Desktop\VS CODE\ICG_competition\tic-tac-toe_player-agent\Data\Images\crosses'
+        files_1 = os.listdir(r'C:\Users\Trijal Srivastava\OneDrive\Desktop\VS CODE\ICG_competition\tic-tac-toe_player-agent\Data\Images\crosses')
+        image_files_1 = [f for f in files_1 if f.endswith( ('.png'))]
+        random_image_1 = random.choice(image_files_1)
+        image_path_1 = os.path.join(folder_1, random_image_1)
+        return image_path_1
+
+
+def png_to_array(file_path):
+     
+        image = Image.open(file_path).convert('L')   
+
+        image_array = np.array(image)
+
+        return image_array
 
 def generate_image(Array):
-    pass
+    m = len(Array)
+    big_matrix = np.zeros((84, 84))
+    small_matrix = []
+
+    for i in range(m):
+
+            if Array[i] == 1:
+                x = choose_image_1()
+                x_np = png_to_array(x)
+                small_matrix.append(x_np)
+        
+    
+            elif Array[i] == 0:
+                x = choose_image_0()
+                x_np = png_to_array(x)
+                small_matrix.append(x_np)
+
+
+
+            elif Array[i] ==2:
+                x = np.zeros((28,28))
+                small_matrix.append(x)
+
+
+    positions = [(0, 0), (0, 28), (0, 56), (28, 0), (28, 28), (28, 56), (56, 0), (56, 28), (56, 56)]
+
+    for i, (row, col) in enumerate(positions):
+            big_matrix[row:row+28, col:col+28] = small_matrix[i]
+
+    return big_matrix
+
+
+    
 
 def get_pattern(querry_array,Flag):
 
